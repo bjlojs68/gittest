d18e750d50a1c472b55bdd56cd7ca89201d60d56
By <3468920682@qq.com>
2022-09-23 21:39:13 +0800

    master test

--- src/main/java/com/blp/test/hello.java
+++ src/main/java/com/blp/test/hello.java
@@ -6,6 +6,8 @@ public class hello {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("master");
+
 
     }
 }
